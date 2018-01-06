5e90d0a90bf7137f8ddbdb34bc34da4de9737d24
Muhwezi Allan <allan.muhwezi@andela.com>
2018-01-07 02:51:18 +0300

    Lesson 2: Exercise 24: Add scrolling weather and set it up in the textview

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,26 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    private TextView weatherData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
+        weatherData = findViewById(R.id.tv_weather_data);
 
-        // TODO (3) Create an array of Strings that contain fake weather data
+        List<String> weather_list = Arrays.asList(getResources().getStringArray(R.array.dummy_weather_conditions));
 
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        for (String weather: weather_list) {
+            weatherData.append(weather + "\n\n\n");
+        }
     }
 }
